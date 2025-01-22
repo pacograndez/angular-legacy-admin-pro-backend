775c7e197ef0aeb5f84d86ea6648b65d314ca93e
pacograndez <paco9194_gc@hotmail.com>
2025-01-21 22:15:01 -0500

    Fin seccion 9 - CORS y Express

---
+++ README.md
@@ -0,0 +1,5 @@
+# AngularLegacyAdminProBackend
+
+## Development server
+
+Run `npm run start:dev` for a dev server. Navigate to `http://localhost:3005/`. The app will automatically reload if you change any of the source files.
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hola Mundo!');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Create Express Server
+const app = express();
+
+//CORS Configuration
+app.use(cors());
+
+// Database
+dbConnection();
+
+//Routes
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Servido corriendo en puerto:', process.env.PORT);
+});
